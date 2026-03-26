00a848a9327371a98cadb35b2d709e031af72c66
Maksim Buldau <maxbuldov@gmail.com>
2026-03-25 22:47:26 -0700

    update deploy

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ const queryClient = new QueryClient();
 export const App = () => {
   return (
     <QueryClientProvider client={queryClient}>
-      <Container />;
+      <Container />
     </QueryClientProvider>
   );
 };
